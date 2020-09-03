610d627b0ed65b81378511b8363ff8e011606120
noob <shelcia1999@gmail.com>
2020-09-03 22:50:59 +0530

    update

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,35 +1,70 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import queryString from "query-string";
 import io from "socket.io-client";
-const ENDPOINT = "localhost:8000";
+
+import TextContainer from "./TextContainer";
+import Messages from "./Messages";
+import InfoBar from "./InfoBar";
+import Input from "./Input";
+
+// import './Chat.css';
+
+let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = "https://project-chat-application.herokuapp.com/";
 
   useEffect(() => {
-    // const data = queryString.parse(location.search);
-    // console.log(location.search);
-    // console.log(data);
     const { name, room } = queryString.parse(location.search);
-    let socket = io(ENDPOINT);
-    console.log(socket);
-    setName(name);
+
+    socket = io(ENDPOINT);
+
     setRoom(room);
-    socket.emit("join", { name, room }, ({ error }) => {
-      alert(error);
+    setName(name);
+
+    socket.emit("join", { name, room }, (error) => {
+      if (error) {
+        alert(error);
+      }
     });
-    return () => {
-      socket.emit("disconnect");
-      socket.off();
-    };
-  }, [location.search]);
+  }, [ENDPOINT, location.search]);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages((messages) => [...messages, message]);
+    });
+
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+
   return (
-    <React.Fragment>
-      <h1>Chat</h1>
-      <h1>hi</h1>
-    </React.Fragment>
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+      <TextContainer users={users} />
+    </div>
   );
 };
 

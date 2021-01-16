3a5835c0b4923633e8ad886b51b5ca88c6a3a7cc
shelcia <shelcia1999@gmail.com>
2021-01-16 11:30:03 +0530

    update styles

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useRef } from "react";
 import { Container, Form, Button } from "react-bootstrap";
 import { v4 as uuidV4 } from "uuid";
-import Chatting from "../assets/chatting.png";
 
 export default function Login({ onIdSubmit }) {
   const idRef = useRef();
@@ -18,15 +17,21 @@ export default function Login({ onIdSubmit }) {
   return (
     <Container className="mt-5 login">
       <div className="row">
-        <div className="col-sm-8">
-          <img src={Chatting} alt="" />
+        <div className="col-sm-8 d-flex align-items-center">
+          <img
+            src="https://cdn.discordapp.com/attachments/795010536365752320/799761776824418354/bg4.png"
+            alt=""
+            style={{ maxHeight: "100vh", maxWidth: "100%" }}
+          />
         </div>
-        <div className="col-sm-4">
-          <h3>Freee Chat App</h3>
-          <p>No Signup Required</p>
-          <Form onSubmit={handleSubmit} className="w-100">
+        <div className="col-sm-4 d-flex align-items-center flex-column justify-content-center">
+          <div className="pl-0">
+            <h1 className="text-white pl-0">Freee Chat App</h1>
+            <p className="text-white pl-0">No Signup Required</p>
+          </div>
+          <Form onSubmit={handleSubmit} className="w-100 mt-3">
             <Form.Group>
-              <Form.Label>Enter Your Id</Form.Label>
+              <Form.Label className="text-white">Enter Your Id</Form.Label>
               <Form.Control
                 type="text"
                 ref={idRef}
